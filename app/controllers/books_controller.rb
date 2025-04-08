78a5333cd9999e4d567d4d15659950d4fc6cc264
sojiro takagi <soujirou.takagi@gmail.com>
2025-04-09 01:54:23 +0900

    Lint修正3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,6 @@ class BooksController < ApplicationController
   def search
     @book = Book.new
       if params[:search].nil?
-        return
       elsif params[:search].blank?
         flash.now[:danger] = "検索キーワードが入力されていません"
       else
@@ -15,23 +14,22 @@ class BooksController < ApplicationController
 
   def create
     @book = current_user.books.build(book_params)
-    #モデルに書いたsave_with_authorメソッドを実行する
+    # モデルに書いたsave_with_authorメソッドを実行する
     if @book.save_with_author(authors_params[:authors])
-      redirect_to books_path, success: t('.success')
+      redirect_to books_path, success: t(".success")
     else
-      flash.now[:danger] = t('.fail')
+      flash.now[:danger] = t(".fail")
     end
   end
 
-  private  
+  private
 
   def book_params
     params.require(:book).permit(:title, :image_link, :info_link, :published_date, :systemid)
   end
-  
-  #authorsは配列で渡ってくるので、配受け取れるように記載
+
+  # authorsは配列で渡ってくるので、配受け取れるように記載
   def authors_params
     params.require(:book).permit(authors: [])
   end
-
 end
