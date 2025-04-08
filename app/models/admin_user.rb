0abeeb0bcaa7d365e9275437fc1b8c1cd64546a6
sojiro takagi <soujirou.takagi@gmail.com>
2025-04-09 02:00:08 +0900

    rubocop設定変更

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,11 +1,11 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+        :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   def self.ransackable_attributes(auth_object = nil)
